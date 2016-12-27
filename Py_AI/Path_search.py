53991144fbd55620726e105f76b0652826401bf5
chaitanyaPaikara <chaitanya1445@gmail.com>
2016-12-27 23:36:09 +0530

    Search Path needing traversal function

--- Py_AI/Path_search.py
+++ Py_AI/Path_search.py
@@ -15,15 +15,24 @@
 #   0 = Navigable space
 #   1 = Occupied space
 
+
 grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
         [0, 0, 1, 0, 1, 0],
         [0, 0, 1, 0, 1, 0],
-        [0, 0, 1, 0, 1, 0]]
+        [0, 0, 0, 0, 1, 0]]
+'''
+grid = [[0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 1, 0]]
+'''
 init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
 unit_cost = 1
 check = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+expand = [[-1 for row in range(len(grid[0]))] for col in range(len(grid))]
 action = [[' ' for col in range(len(grid[0]))] for row in range(len(grid))]
 action[0][0] = 's' 
 action[len(grid)-1][len(grid[0])-1] = 'g' 
@@ -36,24 +45,32 @@ delta_name = ['^', '<', 'v', '>']
 
 def search(grid,init,goal,unit_cost):
     path = []
+    time = 0
     init.append(0)
     path.append(init)
     check[path[0][0]][path[0][1]] = 1
     while path:
-        print path     
+        print path, "\t"+str(time)    
         for j in delta:
             new = [path[0][0] + j[0],path[0][1] + j[1],path[0][2]]
             if new[0] >= 0 and new[1] >= 0 and new[0] < 5 and new[1] < 6:
                 if check[new[0]][new[1]] is 0 and grid[new[0]][new[1]] is 0:
                     if new[0] == goal[0] and new[1] == goal[1]:
                         new[2]+=unit_cost
+                        expand[new[0]][new[1]] = time
                         return new
                     else:
                         new[2]+=unit_cost
                         path.append(new)
-                        action[new[0]][new[1]] = j[2]
+                        #action[new[0]][new[1]] = j[2]
+                        action[path[0][0]][path[0][1]] = j[2]
                         check[new[0]][new[1]] = 1
+                elif grid[new[0]][new[1]] is 1:
+                    expand[new[0]][new[1]] = -1
         del path[0]   
+        expand[path[0][0]][path[0][1]] = time
+        time+=unit_cost
+
 '''
 def traversal(action):
     current = init
@@ -67,4 +84,6 @@ def traversal(action):
 
 print search(grid,init,goal,unit_cost)
 for z in action:
+    print z
+for z in expand:
     print z
