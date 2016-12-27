5b1592844670f6e6bf1da4006948d2587cf844fd
chaitanyaPaikara <chaitanya1445@gmail.com>
2016-12-27 20:05:54 +0530

    Path Traversal Function

--- Py_AI/Path_search.py
+++ Py_AI/Path_search.py
@@ -16,36 +16,31 @@
 #   1 = Occupied space
 
 grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0],
-        [0, 0, 1, 1, 1, 0],
-        [0, 0, 0, 0, 1, 0]]
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0]]
 init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
 unit_cost = 1
-check = [[0,0,0,0,0,0],
-         [0,0,0,0,0,0],
-         [0,0,0,0,0,0],
-         [0,0,0,0,0,0],
-         [0,0,0,0,0,0]]
-
-delta = [[-1, 0], # go up
-         [ 0,-1], # go left
-         [ 1, 0], # go down
-         [ 0, 1]] # go right
+check = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+action = [[' ' for col in range(len(grid[0]))] for row in range(len(grid))]
+action[0][0] = 's' 
+action[len(grid)-1][len(grid[0])-1] = 'g' 
+delta = [[-1, 0,'^'], # go up
+         [ 0,-1,'<'], # go left
+         [ 1, 0,'v'], # go down
+         [ 0, 1,'>']] # go right
 
 delta_name = ['^', '<', 'v', '>']
 
 def search(grid,init,goal,unit_cost):
-    # ----------------------------------------
-    # insert code here
-    # ----------------------------------------
     path = []
-    cost = 0
     init.append(0)
     path.append(init)
     check[path[0][0]][path[0][1]] = 1
     while path:
+        print path     
         for j in delta:
             new = [path[0][0] + j[0],path[0][1] + j[1],path[0][2]]
             if new[0] >= 0 and new[1] >= 0 and new[0] < 5 and new[1] < 6:
@@ -56,7 +51,20 @@ def search(grid,init,goal,unit_cost):
                     else:
                         new[2]+=unit_cost
                         path.append(new)
+                        action[new[0]][new[1]] = j[2]
                         check[new[0]][new[1]] = 1
-        del path[0]          
+        del path[0]   
+'''
+def traversal(action):
+    current = init
+    while current is not goal:
+        for j in delta:
+            new = [current[0] + j[0],current[1] + j[1]]
+            if new[0] >= 0 and new[1] >= 0 and new[0] < 5 and new[1] < 6:
+                if action[new[0]][new[1]] is not ' ':
+                    
+'''
 
 print search(grid,init,goal,unit_cost)
+for z in action:
+    print z
