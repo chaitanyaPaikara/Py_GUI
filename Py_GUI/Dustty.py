dd317418646a012e6e0245cfae09a128f997c587
chaitanyaPaikara <chaitanya1445@gmail.com>
2017-04-05 18:55:51 +0530

    LCD-Widget

---
+++ Py_GUI/Dustty.py
@@ -0,0 +1,46 @@
+from Tkinter import *
+window = Tk()
+window.configure(width=640,height=380,background="#f4f3f1",bd=20,highlightbackground="green", highlightcolor="turquoise4", highlightthickness=5)#"#a1dbcd"  "#202d4e"
+window.title("Segre-G-A-TOR")
+def Click():
+    global last, theLabel
+    theLabel.pack_forget()
+    string = ent.get()
+    if string=='Metal':
+        present = w_metal
+    elif string=='Organic':
+        present = w_organic
+    elif string=='Plastic':
+        present = w_plastic
+    #present.grid(row=0, column=0)
+    present.pack(side = LEFT)
+    last.pack_forget() #last.grid_forget()
+    last = present
+
+    theLabel = Label(window, text=string, font=("Helvetica", 50), fg="#11dbcd", bg="#f4f3f1")
+      # theLabel.grid_forget()
+    theLabel.pack(side = RIGHT)#theLabel.grid(row=0, column=1)
+    '''
+    else:
+        theLabel = Label(window, text="SHIT!!!", font=("Comic sans", 16), fg="#11dbcd", bg="#202d4e")
+        theLabel.grid(row=0,column=0)
+    '''
+btn = Button(window,text="Accept",fg="#11dbcd", bg="#383a39",command=Click)
+global ent, w_metal, w_organic, w_plastic, theLabel, last
+img = PhotoImage(file="Best.gif",width=640,height=175)
+theLabel = Label(window, image=img, width=640, height=175)
+theLabel.pack(fill=X)
+last = Label(window, text="ADD WASTE", font=("Comic sans", 16), fg="#11dbcd", bg="#f4f3f1")
+last.pack()
+img_metal = PhotoImage(file="metal.gif")
+img_orgaic = PhotoImage(file="organic.gif")
+img_plastic = PhotoImage(file="plastic.gif")
+w_metal = Label(window, image=img_metal)
+w_organic = Label(window, image=img_orgaic)
+w_plastic = Label(window, image=img_plastic)
+ent = Entry(window)
+btn.pack(side = BOTTOM)
+ent.pack(side = BOTTOM)
+
+window.geometry('640x480')
+window.mainloop()
--- Py_GUI/Mouse_CLICK.py
+++ Py_GUI/Mouse_CLICK.py
@@ -2,15 +2,20 @@ from Tkinter import *
 
 root = Tk()
 def leftclick(evenet):
+    global theLabel
+    theLabel = Label(root, text="Hello How are you",font=("Helvetica", 16), fg="SlateBlue4", bg="DarkOrchid4")
+    theLabel.grid(row=0, column=0)
     print("Mouse Left Click")
 def middleclick(event):
     print("Mouse Middle Click")
 def rightclick(event):
+    #theLabel.config(font=("Courier", 44))
+    theLabel.grid_forget()
     print("Mouse Right Click")
 
-frame = Frame(root, width=300, height=700)
+frame = Frame(root, width=320, height=240, bg="DarkOrchid4")
 frame.bind("<Button-1>",leftclick)
 frame.bind("<Button-2>",middleclick)
 frame.bind("<Button-3>",rightclick)
-frame.pack()
+frame.grid(row=0,column=0)
 root.mainloop()
