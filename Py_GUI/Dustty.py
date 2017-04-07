08c5a76d6133824b98221fda3c5a1e7ef193d8b0
chaitanyaPaikara <chaitanya1445@gmail.com>
2017-04-07 18:31:26 +0530

    Chn

--- Py_GUI/Dustty.py
+++ Py_GUI/Dustty.py
@@ -1,6 +1,6 @@
 from Tkinter import *
 window = Tk()
-window.configure(width=640,height=380,background="#f4f3f1",bd=20,highlightbackground="green", highlightcolor="turquoise4", highlightthickness=5)#"#a1dbcd"  "#202d4e"
+window.configure(width=320,height=240,background="#f4f3f1",bd=20,highlightbackground="green", highlightcolor="turquoise4", highlightthickness=5)#"#a1dbcd"  "#202d4e"
 window.title("Segre-G-A-TOR")
 def Click():
     global last, theLabel
@@ -27,8 +27,8 @@ def Click():
     '''
 btn = Button(window,text="Accept",fg="#11dbcd", bg="#383a39",command=Click)
 global ent, w_metal, w_organic, w_plastic, theLabel, last
-img = PhotoImage(file="Best.gif",width=640,height=175)
-theLabel = Label(window, image=img, width=320, height=105)
+img = PhotoImage(file="resize_Best.gif",width=320,height=88)
+theLabel = Label(window, image=img, width=320, height=88)
 theLabel.pack(fill=X)
 last = Label(window, text="ADD WASTE", font=("Comic sans", 16), fg="#11dbcd", bg="#f4f3f1")
 last.pack()
